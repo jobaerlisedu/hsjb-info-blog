3a47d687e8bb4ab45f390f9b380b2abb27658408
jobaerlisedu <hsjobaer.lis.edu@outlook.com>
2026-02-12 04:29:16 +0600

    Theme updated.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-// Mobile Menu Toggle
-const menuBtn = document.querySelector('.mobile-menu-btn');
-const nav = document.querySelector('.main-nav');
-
-if (menuBtn) {
-    menuBtn.addEventListener('click', () => {
-        nav.classList.toggle('active');
-
-        // Change icon
-        const icon = menuBtn.querySelector('i');
-        if (nav.classList.contains('active')) {
-            icon.classList.remove('bx-menu');
-            icon.classList.add('bx-x');
-        } else {
-            icon.classList.remove('bx-x');
-            icon.classList.add('bx-menu');
-        }
-    });
-}
+// Main JS File
+// Currently empty as mobile menu script was removed.
+// Future interactive functionality can be added here.
